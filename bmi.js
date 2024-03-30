c2fecc17f60633e43b7d4f9b00f497dfd14d090c
prudorexa <mathuprudence24@gmail.com>
2024-03-30 13:38:50 +0300

    add heading

--- bmi.js
+++ bmi.js
@@ -1,11 +1,10 @@
-
-async function fetchBmiData() {
-const url = 'https://body-mass-index-bmi-calculator.p.rapidapi.com/metric?weight=150&height=1.83';
+async function fetchBmiData () {
+const url = 'https://fitness-calculator.p.rapidapi.com/dailycalorie?age=25&gender=male&height=180&weight=70&activitylevel=level_1';
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': 'd1149e1416msh198f6fe8182459cp10bc36jsn4768637519c9',
-		'X-RapidAPI-Host': 'body-mass-index-bmi-calculator.p.rapidapi.com'
+		'X-RapidAPI-Host': 'fitness-calculator.p.rapidapi.com'
 	}
 };
 
@@ -17,4 +16,5 @@ try {
 	console.error(error);
 }
 }
+
 fetchBmiData();
---
+++ sign in.js
@@ -0,0 +1,79 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCIPcA7rQl5c3LqD6LKlo-pD9jw85xmQic",
+  authDomain: "login-3e500.firebaseapp.com",
+  projectId: "login-3e500",
+  storageBucket: "login-3e500.appspot.com",
+  messagingSenderId: "495574766275",
+  appId: "1:495574766275:web:07b0d49046c26007c4c876"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+auth.languageCode = 'en'
+const provider = new GoogleAuthProvider();
+
+const googleLogin = document.getElementById("google-login-btn");
+googleLogin.addEventListener('click', function () {
+  signInWithPopup(auth, provider)
+  .then((result) => {
+   
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+   
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+   
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+})
+
+const GoogleLogin = document.getElementById("google-signup-btn");
+googleLogin.addEventListener('click', function () {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const Credential = GoogleAuthProvider.CredentialFromResults(result);
+      const user = result.user;
+      console.log(user);
+      window.location.href = "../logged.html";
+
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+
+    });
+
+
+
+
+})
+
+function updateUserProfile(user) {
+  const userName = user.displayName;
+  const userEmail = user.email;
+  const userProfilePicture = user.photoURL;
+
+  document.getElementById("userName").textContent = userName;
+  document.getElementById("useremail").textContent = userEmail;
+  document.getElementById("userProfilePicture").textContent = userProfilePicture;
+}
+
+updateUserProfile
+
+
+
+
+
+
