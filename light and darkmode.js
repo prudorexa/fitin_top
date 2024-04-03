3eba1d6aee2499d03ddccf13a9333b01d3781522
prudorexa <mathuprudence24@gmail.com>
2024-04-03 12:20:55 +0300

    add heading

--- light and darkmode.js
+++ light and darkmode.js
@@ -4,7 +4,6 @@ chk.addEventListener("change", () => {
 	document.body.classList.toggle("dark");
 });
 
-// SOCIAL PANEL JS
 const floating_btn = document.querySelector(".floating-btn");
 const close_btn = document.querySelector(".close-btn");
 const social_panel_container = document.querySelector(
